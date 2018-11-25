fdf12e1ef2a6ee86860283b4151ce567f6d66bbb
Mark Tan <damtan1996@gmail.com>
2018-11-25 10:31:39 -0800

    begin implementation for CNN

--- data_processing_test.py
+++ data_processing_test.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import tensorflow as tf
 from sklearn.metrics import *
 from sklearn.cross_validation import train_test_split #for sklearn 0.20, grab from sklearn.model_selection
+import keras
+import time
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
 import functools
 
 '''
@@ -18,8 +23,6 @@ def disjunction(*values):
 dataframe = pd.read_csv('~/Downloads/clinvar_conflicting.csv')
 row, _ = dataframe.shape
 
-#type dataframe.CHROM as dataframe.loc[:,'CHROM']
-
 #fix chromosome 
 dataframe.loc[:,'CHROM'].replace('X',23, inplace=True)
 dataframe.loc[:,'CHROM'].replace('MT',24, inplace=True)
@@ -27,8 +30,8 @@ dataframe.loc[:,'CHROM'] = pd.to_numeric(dataframe.loc[:,'CHROM'])
 
 #GOAL: convert object column dtypes into string
 #split('/')[row][position = 0, length = 1]
-print(dataframe.loc[:,'EXON'].str.split('/')[0][1])
-print(dataframe.dtypes)
+#print(dataframe.loc[:,'EXON'].str.split('/')[0][1])
+#print(dataframe.dtypes)
 
 #extract start positions for CDNA, CDS, and protein
 dataframe.loc[:,'cDNA_pos_start'] = dataframe.loc[:,'cDNA_position'].str.split('-').str.get(0)
@@ -40,11 +43,7 @@ dataframe.loc[:,'cDNA_pos_start'].replace('?',np.NaN,inplace=True)
 dataframe.loc[:,'CDS_pos_start'].replace('?',np.NaN,inplace=True)
 dataframe.loc[:,'Protein_pos_start'].replace('?',np.NaN,inplace=True)
 
-#initialize start position columns as floats
-dataframe.loc[:,'cDNA_pos_start'] = dataframe.loc[:,'cDNA_pos_start'].astype(float)
-dataframe.loc[:,'CDS_pos_start'] = dataframe.loc[:,'CDS_position'].astype(float)
-dataframe.loc[:,'Protein_pos_start'] = dataframe.loc[:,'Protein_position'].astype(float)
-
+'''
 #mark unspecified CLNDN (DIS_NAME) and create new column
 optionA = dataframe.loc[:,'CLNDN'] == "not_specified"
 optionB = dataframe.loc[:,'CLNDN'] == "not_specified|not_provided"
@@ -52,6 +51,7 @@ optionC = dataframe.loc[:,'CLNDN'] == "not_provided|not_specified"
 optionD = dataframe.loc[:,'CLNDN'] == "not_provided"
 dataframe.loc[disjunction(optionA,optionB,optionC,optionD), 'CLNDN_not_specified'] = 1
 dataframe.loc[:,'CLNDN_not_specified'].fillna(0, inplace=True)
+'''
 
 info = [dataframe.iloc[i,:] for i in range(row)]
 final_data = np.array(info)
@@ -63,3 +63,33 @@ fd_y = final_data[:,18:19]
 #split dataset into training and test data (former has roughly 45k rows while latter has roughly 20k rows)
 train_data, test_data, train_label, test_label = train_test_split(fd_x, fd_y, train_size=0.7,
                                                     random_state=111, stratify=fd_y)
+
+# convert class vectors to binary class matrices
+train_label = keras.utils.to_categorical(train_label, num_classes=None)
+test_label = keras.utils.to_categorical(test_label, num_classes=None)
+
+'''
+input_shape = (10,10,1)
+
+#TEST CONVOLUTIONAL NEURAL NETWORK
+print('CNN TEST: 32 3x3 CONV -> 2x2 MAXPOOL -> softmax')
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+    activation='relu', input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(1, activation='softmax'))
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+start_time = time.time()
+model.fit(train_data, train_label, batch_size=128,
+          epochs=1, verbose=1, validation_data=(test_data, test_label))
+score = model.evaluate(test_data, test_label, verbose=0)
+end_time = time.time()
+total_time = end_time - start_time
+print('Training time:',total_time)
+print('Test accuracy:', score[1])
+'''
