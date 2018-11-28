12bf747b62ad63a39e27777b96319a44b2f94e0c
Mark Tan <damtan1996@gmail.com>
2018-11-28 10:46:14 -0800

    data processing functions complete
    some original columns are deleted because the new columns contain the same information only it is formatted to better suit python ML methods

--- data_processing_test.py
+++ data_processing_test.py
@@ -11,123 +11,136 @@ from keras.layers import Conv1D, MaxPooling1D
 from keras import backend as K
 import functools
 
-def CategoryColumnChanges(dataframe):
-    return 0
+def DataframeAnd(*conditions):
+    return functools.reduce(np.logical_and, conditions)
 
-def StartEndPositions(dataframe):
-    return 0
+def CategoryColumnChanges(df):
+    #fix chromosome (col 0)
+    df.loc[:,'CHROM'].replace('X', 23, inplace=True)
+    df.loc[:,'CHROM'].replace('MT', 24, inplace=True)
+    df.loc[:,'CHROM'] = pd.to_numeric(df.loc[:,'CHROM'])
+    
+    #fix variant type (col 13)
+    df.loc[:,'CLNVC'].replace('single_nucleotide_variant', 1, inplace=True)
+    df.loc[:,'CLNVC'].replace('Duplication', 2, inplace=True)
+    df.loc[:,'CLNVC'].replace('Deletion', 3, inplace=True)
+    df.loc[:,'CLNVC'].replace('Indel', 4, inplace=True)
+    df.loc[:,'CLNVC'].replace('Inversion', 5, inplace=True)
+    df.loc[:,'CLNVC'].replace('Insertion', 6, inplace=True)
+    df.loc[:,'CLNVC'].replace('Microsatellite', 7, inplace=True)
+    df.loc[:,'CLNVC'] = pd.to_numeric(df.loc[:,'CLNVC'])
+    
+    #fix impact (col 20)
+    df.loc[:,'IMPACT'].replace('MODERATE', 1, inplace=True)
+    df.loc[:,'IMPACT'].replace('LOW', 2, inplace=True)
+    df.loc[:,'IMPACT'].replace('MODIFIER', 3, inplace=True)
+    df.loc[:,'IMPACT'].replace('HIGH', 4, inplace=True)
+    df.loc[:,'IMPACT'] = pd.to_numeric(df.loc[:,'IMPACT'])
+    
+    #fix bam_edit (col 34)
+    df.loc[:,'BAM_EDIT'].replace('OK', 0, inplace=True)
+    df.loc[:,'BAM_EDIT'].replace('FAILED', 1, inplace=True)
+    df.loc[:,'BAM_EDIT'] = pd.to_numeric(df.loc[:,'BAM_EDIT'])
+    
+    #fix sift (col 35)
+    df.loc[:,'SIFT'].replace('deleterious', 1, inplace=True)
+    df.loc[:,'SIFT'].replace('tolerated', 2, inplace=True)
+    df.loc[:,'SIFT'].replace('deleterious_low_confidence', 3, inplace=True)
+    df.loc[:,'SIFT'].replace('tolerated_low_confidence', 4, inplace=True)
+    df.loc[:,'SIFT'] = pd.to_numeric(df.loc[:,'SIFT'])
+    
+    #fix polyphen (col 36)
+    df.loc[:,'PolyPhen'].replace('benign', 1, inplace=True)
+    df.loc[:,'PolyPhen'].replace('probably_damaging', 2, inplace=True)
+    df.loc[:,'PolyPhen'].replace('possibly_damaging', 3, inplace=True)
+    df.loc[:,'PolyPhen'].replace('unknown', 4, inplace=True)
+    df.loc[:,'PolyPhen'] = pd.to_numeric(df.loc[:,'PolyPhen'])
 
-def FindAlleleLengths(dataframe):
-    return 0
+    return df
 
-def ExonAndIntronProcessing(dataframe):
-    return 0
+def StartEndPositions(df):
+    #extract start positions for CDNA, CDS, and protein (cols )
+    df.loc[:,'cDNA_pos_start'] = df.loc[:,'cDNA_position'].str.split('-').str.get(0)
+    df.loc[:,'CDS_pos_start'] = df.loc[:,'CDS_position'].str.split('-').str.get(0)
+    df.loc[:,'Protein_pos_start'] = df.loc[:,'Protein_position'].str.split('-').str.get(0)
+    
+    #replace ? with null values in start position columns
+    df.loc[:,'cDNA_pos_start'].replace('?',np.NaN,inplace=True)
+    df.loc[:,'CDS_pos_start'].replace('?',np.NaN,inplace=True)
+    df.loc[:,'Protein_pos_start'].replace('?',np.NaN,inplace=True)
+    
+    #convert start position entries into floats
+    df.loc[:,'cDNA_pos_start'] = pd.to_numeric(df.loc[:,'cDNA_pos_start'])
+    df.loc[:,'CDS_pos_start'] = pd.to_numeric(df.loc[:,'CDS_pos_start'])
+    df.loc[:,'Protein_pos_start'] = pd.to_numeric(df.loc[:,'Protein_pos_start'])
+    
+    #extract end positions for CDNA, CDS, and protein (cols )
+    df.loc[:,'cDNA_pos_end'] = df.loc[:,'cDNA_position'].str.split('-').str.get(1)
+    df.loc[:,'CDS_pos_end'] = df.loc[:,'CDS_position'].str.split('-').str.get(1)
+    df.loc[:,'Protein_pos_end'] = df.loc[:,'Protein_position'].str.split('-').str.get(1)
+    
+    #replace ? with null values in end position columns
+    df.loc[:,'cDNA_pos_end'].replace('?',np.NaN,inplace=True)
+    df.loc[:,'CDS_pos_end'].replace('?',np.NaN,inplace=True)
+    df.loc[:,'Protein_pos_end'].replace('?',np.NaN,inplace=True)
+    
+    #convert start position entries into floats
+    df.loc[:,'cDNA_pos_end'] = pd.to_numeric(df.loc[:,'cDNA_pos_end'])
+    df.loc[:,'CDS_pos_end'] = pd.to_numeric(df.loc[:,'CDS_pos_end'])
+    df.loc[:,'Protein_pos_end'] = pd.to_numeric(df.loc[:,'Protein_pos_end'])
+    
+    return df
+
+def FindAlleleLengths(df):
+    #extract lengths for REF, ALT, Allele (cols )
+    df.loc[:,'REF_len'] = df.loc[:,'REF'].str.len()
+    df.loc[:,'ALT_len'] = df.loc[:,'ALT'].str.len()
+    df.loc[:,'Allele_len'] = df.loc[:,'Allele'].str.len()
+    
+    #convert data types  of columns to float
+    df.loc[:,'REF_len'] = pd.to_numeric(df.loc[:,'REF_len'])
+    df.loc[:,'ALT_len'] = pd.to_numeric(df.loc[:,'ALT_len'])
+    df.loc[:,'Allele_len'] = pd.to_numeric(df.loc[:,'Allele_len'])
+    
+    return df
+
+def ExonIntronProcessing(df):
+    #detect exon/intron presence
+    df.loc[:,'Exon_found'] = df.loc[:,'EXON'].notnull().astype(int)
+    df.loc[:,'Intron_found'] = df.loc[:,'INTRON'].notnull().astype(int)
+    
+    #find exon position
+    df.loc[:,'Exon_pos'] = df.loc[:,'EXON'].str.split('/').str.get(0)
+    df.loc[:,'Exon_pos'] = pd.to_numeric(df.loc[:,'Exon_pos'])
+    
+    #find exon length
+    df.loc[:,'Exon_length'] = df.loc[:,'EXON'].str.split('/').str.get(1)
+    df.loc[:,'Exon_length'] = pd.to_numeric(df.loc[:,'Exon_length'])
+    
+    #turn null pos and length values into zero values
+    exon_pos_empty = df.loc[:,'Exon_pos'].isnull()
+    exon_length_empty = df.loc[:,'Exon_length'].isnull()
+    exon_filter = DataframeAnd(exon_pos_empty, exon_length_empty)
+    df.loc[exon_filter, 'Exon_pos'] = 0
+    df.loc[exon_filter, 'Exon_length'] = 0
+    
+    return df
 
 #process csv file that we will use for project
 dataframe = pd.read_csv('~/Downloads/clinvar_conflicting.csv')
 row, _ = dataframe.shape
 
-
-'''
-CATEGORICAL COLUMN CHANGES
-'''
-
-#fix chromosome (col 0)
-dataframe.loc[:,'CHROM'].replace('X', 23, inplace=True)
-dataframe.loc[:,'CHROM'].replace('MT', 24, inplace=True)
-dataframe.loc[:,'CHROM'] = pd.to_numeric(dataframe.loc[:,'CHROM'])
-
-#fix variant type (col 13)
-dataframe.loc[:,'CLNVC'].replace('single_nucleotide_variant', 1, inplace=True)
-dataframe.loc[:,'CLNVC'].replace('Duplication', 2, inplace=True)
-dataframe.loc[:,'CLNVC'].replace('Deletion', 3, inplace=True)
-dataframe.loc[:,'CLNVC'].replace('Indel', 4, inplace=True)
-dataframe.loc[:,'CLNVC'].replace('Inversion', 5, inplace=True)
-dataframe.loc[:,'CLNVC'].replace('Insertion', 6, inplace=True)
-dataframe.loc[:,'CLNVC'].replace('Microsatellite', 7, inplace=True)
-dataframe.loc[:,'CLNVC'] = pd.to_numeric(dataframe.loc[:,'CLNVC'])
-
-#fix impact (col 20)
-dataframe.loc[:,'IMPACT'].replace('MODERATE', 1, inplace=True)
-dataframe.loc[:,'IMPACT'].replace('LOW', 2, inplace=True)
-dataframe.loc[:,'IMPACT'].replace('MODIFIER', 3, inplace=True)
-dataframe.loc[:,'IMPACT'].replace('HIGH', 4, inplace=True)
-dataframe.loc[:,'IMPACT'] = pd.to_numeric(dataframe.loc[:,'IMPACT'])
-
-#fix bam_edit (col 34)
-dataframe.loc[:,'BAM_EDIT'].replace('OK', 0, inplace=True)
-dataframe.loc[:,'BAM_EDIT'].replace('FAILED', 1, inplace=True)
-dataframe.loc[:,'BAM_EDIT'] = pd.to_numeric(dataframe.loc[:,'BAM_EDIT'])
-
-#fix sift (col 35)
-dataframe.loc[:,'SIFT'].replace('deleterious', 1, inplace=True)
-dataframe.loc[:,'SIFT'].replace('tolerated', 2, inplace=True)
-dataframe.loc[:,'SIFT'].replace('deleterious_low_confidence', 3, inplace=True)
-dataframe.loc[:,'SIFT'].replace('tolerated_low_confidence', 4, inplace=True)
-dataframe.loc[:,'SIFT'] = pd.to_numeric(dataframe.loc[:,'SIFT'])
-
-#fix polyphen (col 36)
-dataframe.loc[:,'PolyPhen'].replace('benign', 1, inplace=True)
-dataframe.loc[:,'PolyPhen'].replace('probably_damaging', 2, inplace=True)
-dataframe.loc[:,'PolyPhen'].replace('possibly_damaging', 3, inplace=True)
-dataframe.loc[:,'PolyPhen'].replace('unknown', 4, inplace=True)
-dataframe.loc[:,'PolyPhen'] = pd.to_numeric(dataframe.loc[:,'PolyPhen'])
-
-
-'''
-CREATE COLUMNS FOR START AND END POSITIONS
-'''
-
-#extract start positions for CDNA, CDS, and protein (cols )
-dataframe.loc[:,'cDNA_pos_start'] = dataframe.loc[:,'cDNA_position'].str.split('-').str.get(0)
-dataframe.loc[:,'CDS_pos_start'] = dataframe.loc[:,'CDS_position'].str.split('-').str.get(0)
-dataframe.loc[:,'Protein_pos_start'] = dataframe.loc[:,'Protein_position'].str.split('-').str.get(0)
-
-#replace ? with null values in start position columns
-dataframe.loc[:,'cDNA_pos_start'].replace('?',np.NaN,inplace=True)
-dataframe.loc[:,'CDS_pos_start'].replace('?',np.NaN,inplace=True)
-dataframe.loc[:,'Protein_pos_start'].replace('?',np.NaN,inplace=True)
-
-#convert start position entries into floats
-dataframe.loc[:,'cDNA_pos_start'] = pd.to_numeric(dataframe.loc[:,'cDNA_pos_start'])
-dataframe.loc[:,'CDS_pos_start'] = pd.to_numeric(dataframe.loc[:,'CDS_pos_start'])
-dataframe.loc[:,'Protein_pos_start'] = pd.to_numeric(dataframe.loc[:,'Protein_pos_start'])
-
-
-#extract end positions for CDNA, CDS, and protein (cols )
-dataframe.loc[:,'cDNA_pos_end'] = dataframe.loc[:,'cDNA_position'].str.split('-').str.get(1)
-dataframe.loc[:,'CDS_pos_end'] = dataframe.loc[:,'CDS_position'].str.split('-').str.get(1)
-dataframe.loc[:,'Protein_pos_end'] = dataframe.loc[:,'Protein_position'].str.split('-').str.get(1)
-
-#replace ? with null values in end position columns
-dataframe.loc[:,'cDNA_pos_end'].replace('?',np.NaN,inplace=True)
-dataframe.loc[:,'CDS_pos_end'].replace('?',np.NaN,inplace=True)
-dataframe.loc[:,'Protein_pos_end'].replace('?',np.NaN,inplace=True)
-
-#convert start position entries into floats
-dataframe.loc[:,'cDNA_pos_end'] = pd.to_numeric(dataframe.loc[:,'cDNA_pos_end'])
-dataframe.loc[:,'CDS_pos_end'] = pd.to_numeric(dataframe.loc[:,'CDS_pos_end'])
-dataframe.loc[:,'Protein_pos_end'] = pd.to_numeric(dataframe.loc[:,'Protein_pos_end'])
-
-
-'''
-FIND REF, ALT, ALLELE LENGTHS
-'''
-
-#extract lengths for REF, ALT, Allele (cols )
-dataframe.loc[:,'REF_len'] = dataframe.loc[:,'REF'].str.len()
-dataframe.loc[:,'ALT_len'] = dataframe.loc[:,'ALT'].str.len()
-dataframe.loc[:,'Allele_len'] = dataframe.loc[:,'Allele'].str.len()
-
-dataframe.loc[:,'REF_len'] = pd.to_numeric(dataframe.loc[:,'REF_len'])
-dataframe.loc[:,'ALT_len'] = pd.to_numeric(dataframe.loc[:,'ALT_len'])
-dataframe.loc[:,'Allele_len'] = pd.to_numeric(dataframe.loc[:,'Allele_len'])
+dataframe = CategoryColumnChanges(dataframe)
+dataframe = StartEndPositions(dataframe)
+dataframe = FindAlleleLengths(dataframe)
+dataframe = ExonIntronProcessing(dataframe)
 
 #delete columns containing miscellaneous or redundantinformation 
 dataframe.drop(['CLNDISDB','CLNDISDBINCL','CLNDN','CLNDNINCL','CLNHGVS','CLNSIGINCL',
     'CLNVI','MC','ORIGIN','SSR','SYMBOL','Feature_type','Feature','BIOTYPE','Amino_acids',
     'Codons','DISTANCE','MOTIF_NAME','MOTIF_POS','HIGH_INF_POS','MOTIF_SCORE_CHANGE',
-    'cDNA_position','CDS_position','Protein_position','REF','ALT','Allele'],axis=1,inplace=True)
+    'cDNA_position','CDS_position','Protein_position','REF','ALT','Allele','EXON','INTRON'],
+    axis=1,inplace=True)
 
 print(dataframe.dtypes)
     
